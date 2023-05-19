68822e12c63bbc36e18a387de59dd12d7211d221
Matias Seitour <matias.seitour01@gmail.com>
2023-05-19 11:06:03 -0300

    add new style to infoCharacter component && add animations

--- src/app/components/InfoCharacter.tsx
+++ src/app/components/InfoCharacter.tsx
@@ -18,6 +18,23 @@ export default function InfoCharacter({
       `#image-character__full__${characterSelected?.id}`
     );
 
+    const titleCharacterSelected = document.querySelector(
+      `.character-selected__title__${characterSelected?.id}`
+    );
+    const descriptionCharacterSelected = document.querySelector(
+      `.character-selected__description__${characterSelected?.id}`
+    );
+
+    gsap.fromTo(
+      titleCharacterSelected,
+      { opacity: 0, duration: 1, delay: 0.5 },
+      {
+        opacity: 1,
+        duration: 1.2,
+        delay: 0.5,
+      }
+    );
+
     gsap.fromTo(
       imageFull,
       { opacity: 0, duration: 1 },
@@ -26,12 +43,22 @@ export default function InfoCharacter({
         duration: 1,
       }
     );
+
+    gsap.fromTo(
+      descriptionCharacterSelected,
+      { opacity: 0, duration: 1, delay: 1 },
+      {
+        opacity: 1,
+        duration: 1,
+        delay: 1,
+      }
+    );
   }, [characterSelected?.id]);
   return (
-    <div className="h-[100%] w-auto relative pt-4">
+    <div className="h-[auto] w-full relative flex flex-1 flex-row justify-start items-center pr-4 bg-black px-4 max-w-[1200px]">
       <figure
         id={`image-character__full__${characterSelected?.id}`}
-        className="w-[300px] h-[400px] mr-2 relative"
+        className="w-[500px] h-[400px] mr-2 relative character-container__effect"
       >
         <Image
           src={
@@ -51,11 +78,17 @@ export default function InfoCharacter({
           }}
         />
       </figure>
-      <div className="w-[400px] h-[200px] bg-black pt-4 pl-2">
-        <h4 className="text-white scale-x-[1] scale-y-[1.2] text-xl mb-2 tracking-wider">
+      <div className="w-[auto] h-[100%] pt-4 pl-2 flex-1 relative">
+        <h4
+          className={`text-red-700 scroll-text__effect bg-red-700 scale-x-[1] scale-y-[1.2] opacity-0 text-2xl mb-2 tracking-wider character-selected__title__${characterSelected?.id} mb-4`}
+        >
           {characterSelected?.name}
         </h4>
-        <p>{characterSelected?.description}</p>
+        <p
+          className={`scale-x-[1] scale-y-[1.2] scroll-text__effect bg-white opacity-0 character-selected__description__${characterSelected?.id}`}
+        >
+          {characterSelected?.description}
+        </p>
       </div>
     </div>
   );
