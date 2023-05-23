7d50bda9d557c6610055adb4c52eccd5b1b9c768
Matias Seitour <matias.seitour01@gmail.com>
2023-05-23 17:18:07 -0300

    add responsive styles

--- src/app/components/InfoCharacter.tsx
+++ src/app/components/InfoCharacter.tsx
@@ -53,10 +53,10 @@ export default function InfoCharacter({
     );
   }, [characterSelected?.id]);
   return (
-    <div className="h-[400px] w-screen relative flex flex-1 info-characters__container opacity-0 flex-row justify-start items-center bg-black image-character__effect">
+    <div className="h-[400px] w-screen relative flex flex-1 info-characters__container opacity-0 flex-row justify-start items-center bg-black image-character__effect max-[700px]:flex-col max-[700px]:h-[auto] max-[700px]:min-h-[auto]">
       <figure
         id={`image-character__full__${characterSelected?.id}`}
-        className="w-[500px] h-[550px] mr-2 relative character-container__effect"
+        className="w-[500px] h-[550px] mr-2 relative character-container__effect max-[700px]:w-[300px] max-[700px]:h-[400px]"
       >
         <Image
           src={
@@ -68,6 +68,8 @@ export default function InfoCharacter({
           width="0"
           height="0"
           sizes="100%"
+          quality={100}
+          priority={true}
           style={{
             width: "100%",
             height: "100%",
@@ -80,14 +82,14 @@ export default function InfoCharacter({
           }}
         />
       </figure>
-      <div className=" h-[100%] pt-4 pl-2 flex-1 relative">
+      <div className="h-[100%] pt-4 pl-2 flex-1 relative max-[700px]:h-full max-[700px]:flex max-[700px]:flex-col max-[700px]:gap-[50px] max-[700px]:text-center max-[700px]:pr-2">
         <h4
-          className={`text-red-700 scroll-text__effect bg-red-700 scale-y-[1.2] pl-2 text-4xl mb-2 tracking-wider character-selected__title__${characterSelected?.id} mb-4`}
+          className={`text-red-700 scroll-text__effect bg-red-700 scale-y-[1.2] pl-2 text-4xl mb-2 tracking-wider character-selected__title__${characterSelected?.id} mb-4 max-[700px]:mb-0 max-[700px]:pl-0`}
         >
           {characterSelected?.name}
         </h4>
         <p
-          className={`pl-2 w-[90%] text-xl scale-y-[1.2] scroll-text__effect bg-white character-selected__description__${characterSelected?.id}`}
+          className={`pl-2 w-[90%] text-xl scale-y-[1.2] scroll-text__effect bg-white character-selected__description__${characterSelected?.id} max-[700px]:w-[100%] max-[700px]:pl-0`}
         >
           {characterSelected?.description}
         </p>
