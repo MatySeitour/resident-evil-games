195f577a2dd0d7cddc98ec4c0bf8a87bac3a9cd9
Matias Seitour <matias.seitour01@gmail.com>
2023-05-23 12:14:30 -0300

    add gsap matchMedia animations

--- src/app/components/TitleHome.tsx
+++ src/app/components/TitleHome.tsx
@@ -5,29 +5,45 @@ import SplitType from "split-type";
 
 export default function TitleHome() {
   useEffect(() => {
-    gsap.registerPlugin(SplitType);
-    const myText = new SplitType("#title-home");
-    const archiveText = new SplitType("#archives-title");
+    let mm = gsap.matchMedia();
+    mm.add("(min-width: 800px)", () => {
+      gsap.registerPlugin(SplitType);
+      const myText = new SplitType("#title-home");
+      const archiveText = new SplitType("#archives-title");
 
-    gsap.from("#title-home .char", {
-      x: 0,
-      stagger: 0.05,
-      opacity: 0,
-      duration: 0.1,
-      delay: 2,
+      gsap.from("#title-home .char", {
+        x: 0,
+        stagger: 0.05,
+        opacity: 0,
+        duration: 0.1,
+        delay: 2,
+      });
+
+      gsap.from("#archives-title .char", {
+        x: 0,
+        stagger: 0.03,
+        opacity: 0,
+        duration: 0.1,
+        delay: 4,
+      });
     });
+    mm.add("(max-width: 800px)", () => {
+      gsap.from("#title-home", {
+        opacity: 0,
+        duration: 1,
 
-    gsap.from("#archives-title .char", {
-      x: 0,
-      stagger: 0.03,
-      opacity: 0,
-      duration: 0.1,
-      delay: 4,
+        delay: 2.1,
+      });
+      gsap.from("#archives-title", {
+        opacity: 0,
+        duration: 1,
+        delay: 3.5,
+      });
     });
   }, []);
 
   return (
-    <div className="w-[auto] h-auto absolute top-[60%] translate-y-[-60%] left-0 pl-4 z-[100]">
+    <div className="w-[auto] h-auto absolute top-[60%] translate-y-[-60%] left-0 pl-4 z-[100] max-[700px]:top-[50%]">
       <div className="title-container background-details__title overflow-hidden relative">
         <h1
           id="title-home"
