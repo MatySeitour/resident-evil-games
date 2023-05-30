a36af11d9234649d077bbbcda4ec62210442c589
Matias Seitour <matias.seitour01@gmail.com>
2023-05-30 14:56:16 -0300

    add spin animation && line class

--- src/app/components/Epilogue.tsx
+++ src/app/components/Epilogue.tsx
@@ -70,6 +70,7 @@ const Epilogue = ({ epilogue, id }: PropEpilogue) => {
         ></span>
         <div className="h-full w-[auto] flex justify-center items-center">
           <Image
+            className="animate-spin opacity-0 umbrella-epilogue"
             src={"/umbrella-epilogue.png"}
             alt="umbrella-epilogue"
             width={48}
@@ -81,21 +82,26 @@ const Epilogue = ({ epilogue, id }: PropEpilogue) => {
         ></span>
       </div>
       <div className="flex w-[100%] justify-center items-center pr-3 pl-3">
-        <p className="text-center relative w-[100%] opacity-1 text-epilogue__effect scale-x-[1] scale-y-[1.2] bg-white text-xl max-[870px]:text-center pt-4 max-[870px]:scale-[1]">
+        <p className="text-center text-epilogue relative w-[100%] opacity-0 text-epilogue__effect mb-4 scale-x-[1] scale-y-[1.2] pr-2 bg-white text-xl max-[870px]:text-center pt-4 max-[870px]:scale-[1] max-[870px]:mb-0 max-[870px]:pr-0">
           {epilogue}
         </p>
       </div>
       <div className="h-14 w-full bg-black flex justify-evenly items-center">
-        <div className="h-[2px] w-[40%] bg-red-800"></div>
+        <span
+          className={`line-left__epilogue__${id} h-[2px] w-[0%] bg-red-800 will-change-auto`}
+        ></span>
         <div className="h-full w-[auto] flex justify-center items-center">
           <Image
+            className="animate-spin opacity-0 umbrella-epilogue"
             src={"/umbrella-epilogue.png"}
             alt="umbrella-epilogue"
             width={48}
             height={48}
           />
         </div>
-        <div className="h-[2px] w-[40%] bg-red-800"></div>
+        <span
+          className={`line-right__epilogue__${id} h-[2px] w-[0%] bg-red-800 will-change-auto`}
+        ></span>
       </div>
     </div>
   );
