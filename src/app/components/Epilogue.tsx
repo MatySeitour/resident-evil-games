a1d8eafb3ea1a65a8d761bba56398b804c0263f3
Matias Seitour <matias.seitour01@gmail.com>
2023-05-30 21:26:58 -0300

    add scroll on epilogue

--- src/app/components/Epilogue.tsx
+++ src/app/components/Epilogue.tsx
@@ -81,11 +81,12 @@ const Epilogue = ({ epilogue, id }: PropEpilogue) => {
           className={`line-right__epilogue__${id} h-[2px] w-[0%] bg-red-800 will-change-auto`}
         ></span>
       </div>
-      <div className="flex w-[100%] justify-center items-center pr-3 pl-3">
+      <div className="flex w-[100%] justify-center items-center pr-3 pl-3 max-[870px]:h-[300px] max-[870px]:overflow-y-scroll max-[870px]:inline-block max-[870px]:pb-[24px]">
         <p className="text-center text-epilogue relative w-[100%] opacity-0 text-epilogue__effect mb-4 scale-x-[1] scale-y-[1.2] pr-2 bg-white text-xl max-[870px]:text-center pt-4 max-[870px]:scale-[1] max-[870px]:mb-0 max-[870px]:pr-0">
           {epilogue}
         </p>
       </div>
+      <div className="bg-black relative w-full gradient-epilogue__text"></div>
       <div className="h-14 w-full bg-black flex justify-evenly items-center">
         <span
           className={`line-left__epilogue__${id} h-[2px] w-[0%] bg-red-800 will-change-auto`}
