81a86a89c0d5e8a940d37ecf707534d8404f0ea7
Matias Seitour <matias.seitour01@gmail.com>
2023-06-02 17:40:41 -0300

    style: add comments and remove useless code

--- src/app/components/Epilogue.tsx
+++ src/app/components/Epilogue.tsx
@@ -1,7 +1,4 @@
 import Image from "next/image";
-import { useEffect } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/all";
 
 interface PropEpilogue {
   epilogue: string;
@@ -9,52 +6,6 @@ interface PropEpilogue {
 }
 
 const Epilogue = ({ epilogue, id }: PropEpilogue) => {
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    // `.line-right__epilogue__${id}`;
-
-    // epilogueTitle.forEach((title: any, i: number) => {
-    //   gsap.to(title, {
-    //     scrollTrigger: {
-    //       trigger: title,
-    //       toggleActions: "play none none none",
-    //       //   markers: true,
-    //       start: "0px center",
-
-    //       end: "0px center",
-    //     },
-    //     opacity: 1,
-    //     duration: 0.5,
-    //   });
-    // });
-    // lineRightEpilogue.forEach((title: any, i: number) => {
-    //   gsap.to(title, {
-    //     scrollTrigger: {
-    //       trigger: title,
-    //       toggleActions: "play none none none",
-    //       start: "0px center",
-
-    //       end: "0px center",
-    //     },
-    //     width: "40%",
-    //     duration: 0.5,
-    //   });
-    // });
-
-    // lineLeftEpilogue.forEach((title: any, i: number) => {
-    //   gsap.to(title, {
-    //     scrollTrigger: {
-    //       trigger: title,
-    //       toggleActions: "play none none none",
-    //       markers: true,
-    //     },
-    //     width: "40%",
-    //     duration: 0.5,
-    //   });
-    // });
-  }, []);
-
   return (
     <div className="bg-black h-[auto] w-full pb-10 flex flex-col justify-center items-center gap-4">
       <h5
