5214f7be9851bc42fc71b4d855c7bf9502e8effb
Matias Seitour <matias.seitour01@gmail.com>
2023-06-02 17:36:12 -0300

    style: add comments and remove useless code

--- src/app/hooks/useIntersection.ts
+++ src/app/hooks/useIntersection.ts
@@ -1,18 +1,22 @@
 import { useState, useEffect } from "react";
 
-export default function useIntersection(a: any) {
+export default function useIntersection(element: any) {
+  /* receives by parameter the section in the form of an element */
+
   const [show, setShow] = useState(0);
   useEffect(() => {
     const observer = new window.IntersectionObserver(function (entries) {
       const { isIntersecting } = entries[0];
       if (isIntersecting) {
+        /* if the element is visible, then it takes its id and sets it to the show state*/
         setShow(Number(entries[0].target.id));
+
         observer.disconnect();
       }
     });
 
-    observer.observe(a.current);
-  }, [a]);
+    observer.observe(element.current);
+  }, [element]);
 
   return show;
 }
